413cd8bf68be288a87b045ebf4cb28e98c12622e
Trinh Le <t.le@hpa.org.nz>
2021-06-15 16:57:04 +1200

    wroong code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,10 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
-}
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+  export default(req: NextApiRequest, res: NextApiResponse<Data>) => {
   console.log("i am in back");
-  res.status(200).json({ name: 'John Doe' });
-  return;
+res.status(200).json({ name: 'John Doe' });
+return;
 }
